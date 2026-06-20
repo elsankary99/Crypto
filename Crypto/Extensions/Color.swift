f993a6caa7e52e03acca250b83216b24b2773e49
Ahmed Elsankary <elsankary99@gmail.com>
2026-06-20 15:33:06 +0300

    adding app logo and Colors

---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Ahmed Elsankary on 20/06/2026.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
